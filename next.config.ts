532df2da41badc63955e196ced81e3386780665f
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2026-03-31 19:16:54 -0500

    test tes test

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 import { dirname } from "path";
 import type { NextConfig } from "next";
 import { fileURLToPath } from "url";
@@ -12,7 +14,8 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SUPABASE_URL:
       process.env.NEXT_PUBLIC_SUPABASE_URL ?? process.env.SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY:
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? process.env.SUPABASE_ANON_KEY,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
+      process.env.SUPABASE_ANON_KEY,
   },
 };
 
